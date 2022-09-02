da718d050344937acb8b5952d74768ca83146ad3
Standa Chvila <schvila@gmail.com>
2022-09-02 08:06:01 +0200

    add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@
 //import logo from './logo.svg';
 //import './App.css';
 import Search from './Search';
-
+import FreeSoloCreateOptionDialog from './FreeSoloCreateOptionDialog';
 function App() {
   return (
     <div className="App">
         <div>
           <Search />
+          <FreeSoloCreateOptionDialog />
         </div>
     </div>
   );
