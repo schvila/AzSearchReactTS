99f068a77ced82b6d81e592922dbbde434c883dc
Standa Chvila <schvila@gmail.com>
2022-09-08 16:49:30 +0200

    test rows

--- src/DataGriDemo.tsx
+++ src/DataGriDemo.tsx
@@ -1,39 +1,40 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { field: "id", headerName: "ID", width: 90 },
   {
-    field: 'firstName',
-    headerName: 'First name',
+    field: "firstName",
+    headerName: "First name",
     width: 150,
     editable: true,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
+    field: "lastName",
+    headerName: "Last name",
     width: 150,
     editable: true,
   },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
+    field: "age",
+    headerName: "Age",
+    type: "number",
     width: 110,
     editable: true,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
     sortable: false,
     width: 160,
     valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
   },
 ];
+// https://www.anycodings.com/1questions/205506/material-ui-get-all-rows-from-datagrid
 function useApiRef() {
   const apiRef = React.useRef(null);
   const _columns = React.useMemo(
@@ -44,7 +45,7 @@ function useApiRef() {
         renderCell: (params) => {
           apiRef.current = params.api;
           return null;
-        }
+        },
       }),
     [columns]
   );
@@ -52,15 +53,15 @@ function useApiRef() {
   return { apiRef, columns: _columns };
 }
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
 ];
 
 export default function DataGridDemo() {
@@ -68,20 +69,18 @@ export default function DataGridDemo() {
   const handleClickButton = () => {
     // @ts-ignore: Object is possibly 'null'.
     console.log(apiRef?.current.getRowModels());
-  };  
+  };
   return (
-    <Box sx={{ height: 400, width: '100%' }}>
+    <Box sx={{ height: 400, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        rowsPerPageOptions={[5,10,15]}
+        rowsPerPageOptions={[5, 10, 15]}
         checkboxSelection
         disableSelectionOnClick
         experimentalFeatures={{ newEditingApi: true }}
       />
-  <Button onClick={handleClickButton}>
-        Show data
-    </Button>    
+      <Button onClick={handleClickButton}>Show data</Button>
     </Box>
   );
 }
